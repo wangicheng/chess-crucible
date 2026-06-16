a67a1374eb6e47c49d8a0134b4a07c739fc147af
h_bugw7 <30070315+wangicheng@users.noreply.github.com>
2026-06-16 16:04:32 +0800

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/chess-crucible/',
   // Use ./src as the root directory for source files
   root: './src',
   envDir: '../',
